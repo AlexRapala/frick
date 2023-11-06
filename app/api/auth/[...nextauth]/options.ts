76b5fdd24cf98b3c5d4e582b5ccb1514f86a4ca6
alexrapala <ajrapala@gmail.com>
2023-11-06 08:44:03 -0600

    fixed auth issues

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -3,10 +3,36 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/lib/turso";
+import { Adapter } from "next-auth/adapters";
+import { accounts, users } from "@/drizzle/schema";
+import { and, eq } from "drizzle-orm";
+
+function getAdapter(): Adapter {
+  return {
+    // @ts-ignore
+    ...DrizzleAdapter(db),
+    async getUserByAccount(providerAccountId) {
+      console.log(providerAccountId);
+      const results = await db
+        .select()
+        .from(accounts)
+        .leftJoin(users, eq(users.id, accounts.userId))
+        .where(
+          and(
+            eq(accounts.provider, providerAccountId.provider),
+            eq(accounts.providerAccountId, providerAccountId.providerAccountId)
+          )
+        )
+        .get();
+
+      return results?.user ?? null;
+    },
+  };
+}
 
 export const options: NextAuthOptions = {
   // @ts-ignore
-  adapter: DrizzleAdapter(db),
+  adapter: getAdapter(),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
